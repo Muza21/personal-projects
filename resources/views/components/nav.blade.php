16d2dd0f208b4e7724eb5d49dc8f169c41dcc16d
Muza21 <lasha.muzashvili1@gmail.com>
2025-02-12 20:46:15 +0400

    fix: mobile style

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -2,20 +2,16 @@
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="flex h-16 items-center justify-between">
             <div class="flex items-center">
-                <div class="hidden md:block">
-                    <div class="flex items-baseline space-x-4">
-                    <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                    {{-- Not-selected: rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white --}}
-                    <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Blogs</a>
-                    </div>
+                <div class="flex items-baseline space-x-4">
+                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                {{-- Not-selected: rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white --}}
+                <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Blogs</a>
                 </div>
             </div>
-            <div class="hidden md:block">
                 <div class="ml-4 flex items-center md:ml-6">
                     <a href="/login" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" aria-current="page">Login</a>
                     <a href="/register" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" aria-current="page">Register</a>
                 </div>
-            </div>
         </div>
     </div>
 </nav>
