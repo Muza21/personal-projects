9e16d7cfccb44b231f549ba09ab14f7ff64060a4
Muza21 <lasha.muzashvili1@gmail.com>
2025-02-12 15:18:34 +0400

    feat: add style for crud posts

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,22 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
     <title>Document</title>
 </head>
 <body>
 <div class="min-h-full">
     <x-nav></x-nav>
-  
-    <header class="bg-white shadow">
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Blogs</h1>
-      </div>
-    </header>
+    <x-header></x-header>
     <main>
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         {{ $slot }}
       </div>
     </main>
   </div>
+  <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
+// GET 	/posts 	index 	posts.index
+// GET 	/posts/create 	create 	posts.create
+// POST 	/posts 	store 	posts.store
+// GET 	/posts/{post} 	show 	posts.show
+// GET 	/posts/{post}/edit 	edit 	posts.edit
+// PUT/PATCH 	/posts/{post} 	update 	posts.update
+// DELETE 	/posts/{post} 	destroy 	posts.destroy
+
+// Route::resource('posts', PostController::class);
+
+Route::get('/', function(){
+    return view('posts.edit');
+    // return redirect('/login');
+});
+
 Route::get('/login', function(){
     return view('login');
 });
